aa2cbe48031423db84be495f8f1ed4e7b29acc42
anakarenbrenis <130012717+anakarenbrenis@users.noreply.github.com>
2023-04-05 19:34:51 -0600

    Update calculadora_imc.py

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -13,28 +13,28 @@ IMC = m/est**2
 print(f"IMC: " + str(IMC))
 
 if IMC >= 0  and IMC <= 15.99 :
-    print("Delgadez Severa")
+    print("IMC Delgadez Severa")
 
 elif IMC >= 16.00 and IMC <= 16.99 :
-    print("Delgadez Moderada")
+    print("IMC Delgadez Moderada")
 
 elif IMC >= 17.00 and IMC <= 18.49 :
-    print("Delgadez Leve")
+    print("IMC Delgadez Leve")
 
 elif IMC >= 18.50 and IMC <= 24.99 :
-    print("Normal")
+    print("IMC Normal")
 
 elif IMC >= 25.00 and IMC <= 29.99 :
-    print("Sobrepeso")
+    print("IMC Sobrepeso")
 
 elif IMC >= 30.00 and IMC <= 34.99 :
-    print("Obesidad Leve")
+    print("IMC Obesidad Leve")
 
 elif IMC >= 35.00 and IMC <= 39.00 :
-    print("Obesidad Media")
+    print("IMC Obesidad Media")
 
 elif IMC <= 40.00 :
-    print("Obesidad Morbida")
+    print("IMC Obesidad Morbida")
 
 
 
